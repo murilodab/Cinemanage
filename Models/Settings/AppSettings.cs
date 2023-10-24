4fb06d484c5cc7a57ddb93d2db8b865965645fdc
Murilo Barbosa <muriloab96@gmail.com>
2023-10-24 18:10:27 -0300

    CinemanageSettings and AppSettings created according to the appsettings.json

---
+++ Models/Settings/AppSettings.cs
@@ -0,0 +1,10 @@
+﻿namespace Cinemanage.Models.Settings
+{
+    public class AppSettings
+    {
+        public CinemanageSettings CinemanageSettings { get; set; }
+        public TMBDBSettings TMDBSettings { get; set; }
+
+
+    }
+}
---
+++ Models/Settings/CinemanageSettings.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc.ViewComponents;
+
+namespace Cinemanage.Models.Settings
+{
+    public class CinemanageSettings
+    {
+        public string? TMDBApiKey {  get; set; }
+        public string? DefaultBackdropSize { get; set; }
+        public string? DefaultYoutubeKey {  get; set; }
+        public string? DefaultCastImage {  get; set; }
+        public DefaultCollection? DefaultCollection { get; set; }
+        public DefaultCredentials? DefaultCredentials { get; set; }
+
+    }
+
+    public class DefaultCollection
+    {
+        public string? Name { get; set; }
+        public string? Description { get; set; }
+
+    }
+
+    public class DefaultCredentials
+    {
+        public string? Role { get; set; }
+        public string? Email { get; set; }
+        public string? Password { get; set; }
+    }
+}

e2ac781f512045356d2a6feb22a5b698adb7e650
Murilo Barbosa <muriloab96@gmail.com>
2023-10-25 16:29:42 -0300

    Added DataMapping interface

---
+++ Services/Interfaces/IDataMappingService.cs
@@ -0,0 +1,13 @@
+﻿using Cinemanage.Models.Database;
+using Cinemanage.Models.TMDB;
+
+namespace Cinemanage.Services.Interfaces
+{
+    public interface IDataMappingService
+    {
+        Task<Movie> MapMovieDetailAsync(MovieDetail movie);
+        ActorDetail MapActorDetail(ActorDetail actor);
+
+
+    }
+}

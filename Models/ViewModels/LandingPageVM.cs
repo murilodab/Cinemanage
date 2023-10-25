7cbb2a9685ae6d0aad11f4f48785053774a3fd1a
Murilo Barbosa <muriloab96@gmail.com>
2023-10-25 14:38:22 -0300

    Created the LandingPage View Model

---
+++ Models/ViewModels/LandingPageVM.cs
@@ -0,0 +1,16 @@
+﻿using Cinemanage.Models.Database;
+
+namespace Cinemanage.Models.ViewModels
+{
+    public class LandingPageVM
+    {
+        public List<Collection>? CustomCollections { get; set; }
+        public MovieSearch? NowPlaying { get; set; }
+        public MovieSearch? Popular { get; set; }
+        public MovieSearch? TopRated { get; set; }
+        public MovieSearch? Upcoming { get; set; }
+
+
+
+    }
+}

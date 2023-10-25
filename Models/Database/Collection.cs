2530c9bf0cba9084305daccf6896a615dde8ffa6
Murilo Barbosa <muriloab96@gmail.com>
2023-10-25 11:07:46 -0300

    created enums for ratings created MovieCollection to handle many to many relationship

---
+++ Models/Database/Collection.cs
@@ -0,0 +1,9 @@
+﻿namespace Cinemanage.Models.Database
+{
+    public class Collection
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public string? Descripton {  get; set; }
+    }
+}
---
+++ Models/Database/MovieCollection.cs
@@ -0,0 +1,17 @@
+﻿namespace Cinemanage.Models.Database
+{
+    public class MovieCollection
+    {
+        public int Id {  get; set; }
+        public int CollectionId {  get; set; }
+        public int MovieId {  get; set; }
+
+
+        public int Order {  get; set; }
+
+        public Collection? Collection { get; set; }
+        public Movie? Moview { get; set; }
+
+
+    }
+}

edf7e556fc133cf587bc0b30bff7bc6421905653
Murilo Barbosa <muriloab96@gmail.com>
2023-10-28 11:09:16 -0300

    created ConnectionService to handle database connection to remote server

---
+++ Services/Interfaces/IImageService.cs
@@ -0,0 +1,9 @@
+﻿namespace Cinemanage.Services.Interfaces
+{
+    public interface IImageService
+    {
+        Task<byte[]> EncodeImageAsync(IFormFile poster);
+        Task<byte[]> EncodeImageURLAsync(string imageURL);
+        string DecodeImage(byte[] oster, string contentType);
+    }
+}

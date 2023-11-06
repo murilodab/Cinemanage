df990e1f0677a85b7d2709c5d82b8fa0925cfe6c
Murilo Barbosa <muriloab96@gmail.com>
2023-11-06 11:17:30 -0300

    Created BasicImaggeService using IHttpClientFactory. Needs to write decoding and encoding methods

---
+++ Services/BasicImageService.cs
@@ -0,0 +1,30 @@
+﻿using Cinemanage.Services.Interfaces;
+
+namespace Cinemanage.Services
+{
+    public class BasicImageService : IImageService
+    {
+
+        private readonly IHttpClientFactory _httpClient;
+
+        public BasicImageService(IHttpClientFactory httpClient)
+        {
+            _httpClient = httpClient;
+        }
+
+        public string DecodeImage(byte[] oster, string contentType)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<byte[]> EncodeImageAsync(IFormFile poster)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<byte[]> EncodeImageURLAsync(string imageURL)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
